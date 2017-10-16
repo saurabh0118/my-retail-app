f8abcf1b9a92de377984c52eb6c98061a8ff8395
Saurabh Chauhan <saurabh0118@gmail.com>
2017-10-15 18:49:27 -0700

    Basic Directory Structure Modification

--- src/test/java/com/target/myretail/MyRetailApplicationTests.java
+++ src/test/java/com/target/myretail/MyRetailApplicationTests.java
@@ -1,4 +1,4 @@
-package com.target.myretail.myretailapp;
+package com.target.myretail;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MyRetailAppApplicationTests {
+public class MyRetailApplicationTests {
 
 	@Test
 	public void contextLoads() {
