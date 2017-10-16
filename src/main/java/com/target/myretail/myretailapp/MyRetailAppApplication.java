35431b7412d18d2e0e3a7901eec7524f7836d7ce
Saurabh Chauhan <saurabh0118@gmail.com>
2017-10-15 18:43:11 -0700

    Initial Skeleton of the Retail App

---
+++ src/main/java/com/target/myretail/myretailapp/MyRetailAppApplication.java
@@ -0,0 +1,12 @@
+package com.target.myretail.myretailapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRetailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRetailAppApplication.class, args);
+	}
+}
